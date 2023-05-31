1672cd701a0a4ce3a094087dc3aa51a70db5b720
nko <konstantyn.nazarenko@gmail.com>
2023-05-31 23:29:53 +0300

    add error boundary

---
+++ src/widgets/ErrorPage/ui/ErrorPage.tsx
@@ -0,0 +1,34 @@
+import { FC } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import Button from 'shared/ui/Button/Button';
+import cls from './ErrorPage.module.scss';
+
+interface ErrorPageProps {
+    className?: string;
+}
+
+const ErrorPage: FC<ErrorPageProps> = (props) => {
+    const { className } = props;
+
+    const { t } = useTranslation();
+
+    const reloadPage = () => {
+        // eslint-disable-next-line no-restricted-globals
+        location.reload();
+    };
+
+    return (
+        <div className={classNames(
+            cls.ErrorPage,
+            { },
+            [className],
+        )}
+        >
+            <p>{t('Something went wrong...')}</p>
+            <Button onClick={reloadPage}>{t('Reload page')}</Button>
+        </div>
+    );
+};
+
+export default ErrorPage;
